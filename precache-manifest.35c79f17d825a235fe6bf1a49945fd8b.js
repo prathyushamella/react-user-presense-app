dea8ddf616f29e7e339a3967d9fd359773437d7f
Prathyusha Mella <prathyushamella@gmail.com>
2020-06-14 19:10:25 +0530

    Updates

---
+++ precache-manifest.35c79f17d825a235fe6bf1a49945fd8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d3a296378f6b21721a68d09b8e8e9e",
+    "url": "/react-user-presense-app/index.html"
+  },
+  {
+    "revision": "d2372ac10aa453ec1a58",
+    "url": "/react-user-presense-app/static/css/main.c50f392a.chunk.css"
+  },
+  {
+    "revision": "454e9322ce93f35f2916",
+    "url": "/react-user-presense-app/static/js/2.c1845e68.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/react-user-presense-app/static/js/2.c1845e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2372ac10aa453ec1a58",
+    "url": "/react-user-presense-app/static/js/main.70fdf2ed.chunk.js"
+  },
+  {
+    "revision": "b175ca5e04c582d3b700",
+    "url": "/react-user-presense-app/static/js/runtime-main.34976c3b.js"
+  }
+]);
